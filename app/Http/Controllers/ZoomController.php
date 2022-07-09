462d87faf9f36dadafdc0d656571e0ac41b8b7cd
Eric Muga <eric.muga@gmail.com>
2022-07-09 17:07:18 +0300

    modified expiry of jwt again

--- app/Http/Controllers/ZoomController.php
+++ app/Http/Controllers/ZoomController.php
@@ -26,8 +26,7 @@ class ZoomController extends Controller
                 "iss" =>env('JWT_APP_KEY',''),
                 "exp" =>strtotime("18:29 07/09/2022")
             );
-
-            return 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6IlRUdkUzOW5iUi1LajZrWDdiNXgyc1EiLCJleHAiOjE2NTczODA3MzcsImlhdCI6MTY1NzM3NTMzOH0.jU-oamJAjalVlGv2cLr7Wm7CTVCSyrgD_1zobQqVgeyJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6IlRUdkUzOW5iUi1LajZrWDdiNXgyc1EiLCJleHAiOjE2NTczODA3MzcsImlhdCI6MTY1NzM3NTMzOH0.jU-oamJAjalVlGv2cLr7Wm7CTVCSyrgD_1zobQqVgok';
+           return 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6IlRUdkUzOW5iUi1LajZrWDdiNXgyc1EiLCJleHAiOjE2NTc5ODAzODcsImlhdCI6MTY1NzM3NTU4N30.7mF8iIo4d984ZzD7F10iABDwF5v1zUgnSPECi0jfITk';
            return JWT::encode($payload, $key,'HS256');
             // return 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6IlRUdkUzOW5iUi1LajZrWDdiNXgyc1EiLCJleHAiOjE2NDk5Mzg5ODgsImlhdCI6MTY0OTMzNDE4OH0.So0mJbQAipZi5azENcU1yMNtsbODG4OKvcRdILA0E54';
 
