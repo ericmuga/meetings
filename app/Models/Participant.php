2800a1ee11c6186e33ac17d48f3b86337f8a8b3a
Eric Muga <eric.muga@gmail.com>
2022-07-13 05:31:51 +0300

    added download zoom meeting participants function

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Participant extends Model
 {
     use HasFactory;
+
+    protected $guarded=['id'];
+    public function meeting()
+    {
+        return $this->belongsTo(Meeting::class);
+    }
 }
