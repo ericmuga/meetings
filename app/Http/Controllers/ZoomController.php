6fef046a394ca9ef0b52684726b21a46344ab126
Eric Muga <eric.muga@gmail.com>
2022-07-29 05:38:49 +0300

    testing day of week

--- app/Http/Controllers/ZoomController.php
+++ app/Http/Controllers/ZoomController.php
@@ -241,8 +241,7 @@ class ZoomController extends Controller
 
                         $d=ZoomController::getInstanceDetails($instance);
                         if(!Meeting::where('uuid',$d->uuid)->exists())
-                          if(($d->participants_count>=$rule->minimum_members) && ($d->duration>=$rule->minimum_minutes))
-                           dd(Carbon::parse($d->start_time)->dayOfWeek);
+                          if(($d->participants_count>=$rule->minimum_members) && ($d->duration>=$rule->minimum_minutes) && Carbon::parse($d->start_time)->dayOfWeek==5)
                           Meeting::create([
                                                     'type'=>'zoom',
                                                     'date'=>Carbon::parse($d->start_time)->toDateString(),
