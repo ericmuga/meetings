a8fd4eaf7764c65cdafb0e676a5f12e6deea1484
Eric Muga <eric.muga@gmail.com>
2024-03-09 08:54:45 +0300

    hack for member.data

--- resources/js/Components/MemberCard.vue
+++ resources/js/Components/MemberCard.vue
@@ -6,20 +6,20 @@
             <div class="flex justify-end px-4 pt-4">
 
             </div>
-            <div class="flex flex-col items-center pb-5" v-if="member.id>0">
-                <img class="w-24 h-24 mb-3 rounded-full shadow-lg" :src=member.gravatar alt="Bonnie image">
-                <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{member.name}}</h5>
-                <span class="text-sm text-gray-500 dark:text-gray-400">{{member.field}}</span>
-                <span class="text-sm text-gray-500 dark:text-gray-400">{{member.member_no}}</span>
+            <div class="flex flex-col items-center pb-5" v-if="m.id>0">
+                <img class="w-24 h-24 mb-3 rounded-full shadow-lg" :src=m.gravatar alt="Bonnie image">
+                <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{m.name}}</h5>
+                <span class="text-sm text-gray-500 dark:text-gray-400">{{m.field}}</span>
+                <span class="text-sm text-gray-500 dark:text-gray-400">{{m.member_no}}</span>
                 <div class="flex mt-4 space-x-3 lg:mt-6">
                        <Link
-                          :href="route('member.show',member.id)"
+                          :href="route('member.show',m.id)"
                        >
                             <Button icon="pi pi-user" class="p-button-rounded p-button-info" />
                        </Link>
 
                        <Link
-                          :href="route('member.edit',member.id)"
+                          :href="route('member.edit',m.id)"
                        >
                             <Button icon="pi pi-pencil" class="p-button-rounded p-button-secondary" />
                        </Link>
@@ -29,35 +29,7 @@
                             <!-- <Button icon="pi pi-times" class="justify-end p-button-rounded p-button-danger"
                                     @click="dropMember(member.id)"
                             /> -->
-                        <Drop :dropRoute="route('member.destroy',member.id)" />
-
-
-            </div>
-            </div>
-              <div class="flex flex-col items-center pb-5" v-else>
-                <img class="w-24 h-24 mb-3 rounded-full shadow-lg" :src=member.data.gravatar alt="Bonnie image">
-                <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{member.data.name}}</h5>
-                <span class="text-sm text-gray-500 dark:text-gray-400">{{member.data.field}}</span>
-                <span class="text-sm text-gray-500 dark:text-gray-400">{{member.data.member_no}}</span>
-                <div class="flex mt-4 space-x-3 lg:mt-6">
-                       <Link
-                          :href="route('member.show',member.data.id)"
-                       >
-                            <Button icon="pi pi-user" class="p-button-rounded p-button-info" />
-                       </Link>
-
-                       <Link
-                          :href="route('member.edit',member.data.id)"
-                       >
-                            <Button icon="pi pi-pencil" class="p-button-rounded p-button-secondary" />
-                       </Link>
-
-
-
-                            <!-- <Button icon="pi pi-times" class="justify-end p-button-rounded p-button-danger"
-                                    @click="dropMember(member.id)"
-                            /> -->
-                        <Drop :dropRoute="route('member.destroy',member.data.id)" />
+                        <Drop :dropRoute="route('member.destroy',m.id)" />
 
 
             </div>
@@ -78,6 +50,15 @@ const props= defineProps({
    member:Object,
 
 });
+
+onMounted(()=>{
+    if (props.guest.id>0)
+    {
+     m=props.guest
+    }
+    else
+    m=props.guest.data
+})
 
 
 
